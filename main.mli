802d5587f75777e31096c6c1f5a6b593886c1c42
Al <ap689@cornell.edu>
2020-12-19 11:03:47 -0500

    mailmli

---
+++ main.mli
@@ -0,0 +1,6 @@
+(** A module for initiating the game. Starts the game and lets the 
+    user play *)
+
+(** [go_t] starts the game, and will automatically run when ran with
+    'make play', or module is imported. *)
+val go_t : unit
